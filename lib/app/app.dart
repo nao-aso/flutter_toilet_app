f0d90508c9f58b5e5fa514bf7e827cc5f2e19f2a
taka9076 <takahashi.hiroto.asojuku2022@gmail.com>
2025-10-29 11:53:03 +0900

    カレンダー追加

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import '../shared/widgets/bottom_nav_scaffold.dart';
 
 class ToiletApp extends StatelessWidget {
@@ -9,6 +10,14 @@ class ToiletApp extends StatelessWidget {
     return MaterialApp(
       title: 'Toilet Monitor',
       locale: const Locale('ja', 'JP'), // 日本語ロケール指定
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const [
+        Locale('ja', 'JP'),
+      ],
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         useMaterial3: true,
